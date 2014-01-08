61bacb2a710b94722c79e1bfe337a26ae5852a0b
Oliver Charles <ollie@ocharles.org.uk>
2014-01-08 09:40:08 +0000

    Test HsImport using Cabal test-suite functionality
    Previously HsImport built a "hsimport-tests" executable, but this
    has a few drawbacks. Firstly, one cannot run tests using `cabal test`,
    which is the idiomatic way to run tests. Due to non-standard testing,
    automated packaging for distributions such as NixOS can't be
    performed.
    
    Another reason to prefer test-suite is that test dependencies are
    only required if tests are going to be ran, and the user will not
    end up with a test binary on their $PATH after installation.
    
    To use the test-suite functionality, I've slightly modified how
    tests are ran. Rather than invoke the actual `hsimport` binary,
    the tests now link against HsImport as a library, and call `main`
    manually. This means that we don't quite test the main entry point,
    but we still test the majority of the code. This change was
    necessary as `cabal test` doesn't have `hsimport` in the $PATH,
    and this felt simpler than trying to find the binary or assuming
    it was installed.
    
    This commit also increases the upper-bound on `tasty`.

--- src/HsImport/Args.hs
+++ src/HsImport/Args.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE DeriveDataTypeable, CPP #-}
 
-module HsImport.Args 
+module HsImport.Args
    ( HsImportArgs(..)
    , hsImportArgs
+   , hsImportArgsOptions
    ) where
 
 import System.Console.CmdArgs
@@ -12,7 +13,7 @@ import Data.Version (showVersion)
 import Paths_hsimport (version)
 #endif
 
-data HsImportArgs = HsImportArgs 
+data HsImportArgs = HsImportArgs
    { moduleName    :: String
    , symbolName    :: String
    , qualifiedName :: String
@@ -20,23 +21,24 @@ data HsImportArgs = HsImportArgs
    , outputSrcFile :: FilePath
    } deriving (Data, Typeable, Show, Eq)
 
+hsImportArgsOptions :: HsImportArgs
+hsImportArgsOptions = HsImportArgs
+  { moduleName    = def &= help "The module to import"
+  , symbolName    = def &= help "The symbol to import, if empty, the entire module is imported"
+  , qualifiedName = def &= help "The name to use for a qualified module import"
+  , outputSrcFile = def &= help "Save modified source file to file, if empty, the source file is modified inplace" &= typFile
+  , inputSrcFile  = def &= args &= typ "SOURCEFILE"
+  }
+  &= program "hsimport"
+  &= summary summaryInfo
+  &= help "A command line program for extending the import list of a Haskell source file."
+  &= helpArg [explicit, name "help", name "h"]
+  &= versionArg [explicit, name "version", name "v", summary versionInfo]
+  where
+     summaryInfo = ""
 
 hsImportArgs :: IO HsImportArgs
-hsImportArgs = cmdArgs $ HsImportArgs 
-   { moduleName    = def &= help "The module to import"
-   , symbolName    = def &= help "The symbol to import, if empty, the entire module is imported"
-   , qualifiedName = def &= help "The name to use for a qualified module import"
-   , outputSrcFile = def &= help "Save modified source file to file, if empty, the source file is modified inplace" &= typFile
-   , inputSrcFile  = def &= args &= typ "SOURCEFILE"
-   }
-   &= program "hsimport"
-   &= summary summaryInfo
-   &= help "A command line program for extending the import list of a Haskell source file."
-   &= helpArg [explicit, name "help", name "h"]
-   &= versionArg [explicit, name "version", name "v", summary versionInfo]
-   where
-      summaryInfo = ""
-
+hsImportArgs = cmdArgs hsImportArgsOptions
 
 versionInfo :: String
 versionInfo =
